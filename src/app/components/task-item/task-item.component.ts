4e4310dd4fab15ed8638646801d2d341edec855a
Simon Oser <simon.oser@gmail.com>
2025-04-03 17:35:21 +0200

    Angular FontAwsome Setup

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,14 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../Task';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [FontAwesomeModule, NgStyle],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
 
   @Input() task!: Task;
+  faTimes = faTimes;
+
 }
