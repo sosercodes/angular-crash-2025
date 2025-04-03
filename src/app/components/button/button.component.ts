a98e00da2b8a66c48799edf02f333c9224b462c3
Simon Oser <simon.oser@gmail.com>
2025-04-03 11:42:45 +0200

    Component Input Data

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
 
+  @Input() text!: string;
+  @Input() color!: string;
 }
