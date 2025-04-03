1b962f72c144e6403c70ee11d769dcd08292c5e8
Simon Oser <simon.oser@gmail.com>
2025-04-03 14:28:01 +0200

    Looping with ngFor

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { TASKS } from '../../mock-tasks';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-
+  tasks: Task[] = TASKS;
 }
