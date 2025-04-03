9a40c1dc8cba7bb7bfaa0e7e5c01eab278c2a24d
Simon Oser <simon.oser@gmail.com>
2025-04-03 16:46:16 +0200

    Task Item Component

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../../Task';
+
+
+@Component({
+  selector: 'app-task-item',
+  imports: [],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+
+  @Input() task!: Task;
+}
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { TASKS } from '../../mock-tasks';
 import { Task } from '../../Task';
+import { TaskItemComponent } from '../task-item/task-item.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [NgFor],
+  imports: [NgFor, TaskItemComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
