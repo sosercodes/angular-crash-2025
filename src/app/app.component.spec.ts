23d1f3d84cfdd2d56ef21699bff29139c46b0168
Simon Oser <simon.oser@gmail.com>
2025-04-08 11:02:37 +0200

    Fetching Tasks using Angular HTTP Client

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'angular-crash' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-crash');
   });
 
   it('should render title', () => {
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
 
 @Injectable({
@@ -9,10 +9,11 @@ import { Task } from '../Task';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/tasks';
+
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks =  of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
